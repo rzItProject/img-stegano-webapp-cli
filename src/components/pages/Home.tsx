ddf4a6b73da4ba96c6d506cc318b46d48273f7a7
rzItProject <eabdelrazak.pro@gmail.com>
2023-09-15 02:20:20 +0200

    Protected route & update UI

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,19 +1,11 @@
 import React, { useEffect } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { Navigate, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
-  const { isAuthenticated, isLoading } = useAuth();
-  const navigate = useNavigate();
   const notify = () => toast("Wow so easy!");
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/login");
-    }
-  }, [isAuthenticated, navigate]);
 
   return (
     <div>
---
+++ src/components/ui/user_auth_form.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserAuthForm = () => {
+  return (
+<div></div>
+  )
+}
+
+export default UserAuthForm
